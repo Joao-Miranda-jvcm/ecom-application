b4d4bb1cad3583062358cd709d4d3dd242f62372
Joao-Miranda-jvcm <joaovictorcamargodemiranda@hotmail.com>
2025-08-08 23:23:04 -0300

    feat(product): add service, controller and DTOs

---
+++ src/main/java/com/jvcm/ecom/service/ProductService.java
@@ -0,0 +1,55 @@
+package com.jvcm.ecom.service;
+
+import com.jvcm.ecom.dto.ProductRequest;
+import com.jvcm.ecom.dto.ProductResponse;
+import com.jvcm.ecom.model.Product;
+import com.jvcm.ecom.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = new Product();
+        updateProductFromRequest(product, productRequest);
+        Product savedProduct = productRepository.save(product);
+        return mapToProductResponse(savedProduct);
+    }
+
+    private ProductResponse mapToProductResponse(Product savedProduct) {
+        ProductResponse response = new ProductResponse();
+        response.setId(savedProduct.getId());
+        response.setName(savedProduct.getName());
+        response.setDescription(savedProduct.getDescription());
+        response.setPrice(savedProduct.getPrice());
+        response.setCategory(savedProduct.getCategory());
+        response.setImageUrl(savedProduct.getImageUrl());
+        response.setStockQuantity(savedProduct.getStockQuantity());
+        response.setActive(savedProduct.getActive());
+        return response;
+    }
+
+    private void updateProductFromRequest(Product product, ProductRequest productRequest) {
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setCategory(productRequest.getCategory());
+        product.setImageUrl(productRequest.getImageUrl());
+        product.setStockQuantity(productRequest.getStockQuantity());
+    }
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
+       return productRepository.findById(id)
+                .map(existingProduct -> {
+                    updateProductFromRequest(existingProduct, productRequest);
+                    Product savedProduct = productRepository.save(existingProduct);
+                    return mapToProductResponse(savedProduct);
+                });
+    }
+}
